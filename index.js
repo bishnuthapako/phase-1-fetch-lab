5df918f1fa6f4fba51aa69079447ca916307c812
Bishnu <bishnuthapako@gmail.com>
2022-05-02 18:31:19 -0400

    Completed assigment

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  const books = fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(json => renderBooks(json))
+  return books
   
 }
 
